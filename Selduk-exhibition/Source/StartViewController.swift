4a91088a858504158bb15c5b062dcefa520a2b0d
권준상 <wnstkdsun97@naver.com>
2022-05-02 15:05:04 +0900

    폴더 정리

---
+++ Selduk-exhibition/Source/StartViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Selduk-exhibition
+//
+//  Created by 권준상 on 2022/05/02.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
