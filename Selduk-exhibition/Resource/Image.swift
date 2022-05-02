3d92b52bb0fef8e9f6a293315a45d658fc7b496e
권준상 <wnstkdsun97@naver.com>
2022-05-02 15:48:06 +0900

    이미지 추가

---
+++ Selduk-exhibition/Resource/Image.swift
@@ -0,0 +1,83 @@
+//
+//  Image.swift
+//  Selduk-exhibition
+//
+//  Created by 권준상 on 2022/05/02.
+//
+
+import UIKit.UIImage
+
+enum Image {
+    
+    // 캐릭터 모양
+    static let navyShapeCircle = UIImage(named: "shape_navy_circle")!
+    static let navyShapeCloud = UIImage(named: "shape_navy_cloud")!
+    static let navyShapeBread = UIImage(named: "shape_navy_bread")!
+    static let navyShapeSharpEar = UIImage(named: "shape_navy_sharpEar")!
+    static let navyShapeRoundEar = UIImage(named: "shape_navy_roundEar")!
+    static let navyShapeJjang = UIImage(named: "shape_navy_jjang")!
+    
+    static let yellowShapeCircle = UIImage(named: "shape_yellow_circle")!
+    static let yellowShapeCloud = UIImage(named: "shape_yellow_cloud")!
+    static let yellowShapeBread = UIImage(named: "shape_yellow_bread")!
+    static let yellowShapeSharpEar = UIImage(named: "shape_yellow_sharpEar")!
+    static let yellowShapeRoundEar = UIImage(named: "shape_yellow_roundEar")!
+    static let yellowShapeJjang = UIImage(named: "shape_yellow_jjang")!
+    
+    static let pinkShapeCircle = UIImage(named: "shape_pink_circle")!
+    static let pinkShapeCloud = UIImage(named: "shape_pink_cloud")!
+    static let pinkShapeBread = UIImage(named: "shape_pink_bread")!
+    static let pinkShapeSharpEar = UIImage(named: "shape_pink_sharpEar")!
+    static let pinkShapeRoundEar = UIImage(named: "shape_pink_roundEar")!
+    static let pinkShapeJjang = UIImage(named: "shape_pink_jjang")!
+    
+    static let mauveShapeCircle = UIImage(named: "shape_mauve_circle")!
+    static let mauveShapeCloud = UIImage(named: "shape_mauve_cloud")!
+    static let mauveShapeBread = UIImage(named: "shape_mauve_bread")!
+    static let mauveShapeSharpEar = UIImage(named: "shape_mauve_sharpEar")!
+    static let mauveShapeRoundEar = UIImage(named: "shape_mauve_roundEar")!
+    static let mauveShapeJjang = UIImage(named: "shape_mauve_jjang")!
+    
+    static let greenShapeCircle = UIImage(named: "shape_green_circle")!
+    static let greenShapeCloud = UIImage(named: "shape_green_cloud")!
+    static let greenShapeBread = UIImage(named: "shape_green_bread")!
+    static let greenShapeSharpEar = UIImage(named: "shape_green_sharpEar")!
+    static let greenShapeRoundEar = UIImage(named: "shape_green_roundEar")!
+    static let greenShapeJjang = UIImage(named: "shape_green_jjang")!
+    
+    // 캐릭터 표정바
+    static let expressionBar1 = UIImage(named: "expressionBar1")!
+    static let expressionBar2 = UIImage(named: "expressionBar2")!
+    static let expressionBar3 = UIImage(named: "expressionBar3")!
+    static let expressionBar4 = UIImage(named: "expressionBar4")!
+    static let expressionBar5 = UIImage(named: "expressionBar5")!
+    static let expressionBar6 = UIImage(named: "expressionBar6")!
+    static let expressionBar7 = UIImage(named: "expressionBar7")!
+    static let expressionBar8 = UIImage(named: "expressionBar8")!
+    static let expressionBar9 = UIImage(named: "expressionBar9")!
+    static let expressionBar10 = UIImage(named: "expressionBar10")!
+    static let expressionBar11 = UIImage(named: "expressionBar11")!
+    static let expressionBar12 = UIImage(named: "expressionBar12")!
+    static let expressionBar13 = UIImage(named: "expressionBar13")!
+    static let expressionBar14 = UIImage(named: "expressionBar14")!
+    static let expressionBar15 = UIImage(named: "expressionBar15")!
+    
+    // 특징 바
+    static let featureBarHair1 = UIImage(named: "featureBar_hair1")!
+    static let featureBarHair2 = UIImage(named: "featureBar_hair2")!
+    static let featureBarSleepHat = UIImage(named: "featureBar_sleepHat")!
+    static let featureBarHat = UIImage(named: "featureBar_hat")!
+    static let featureBarFrog = UIImage(named: "featureBar_frog")!
+    static let featureBarSunglasses = UIImage(named: "featureBar_sunglasses")!
+    static let featureBarAngel = UIImage(named: "featureBar_angel")!
+    static let featureBarHeadphone = UIImage(named: "featureBar_headphone")!
+    static let featureBarRibbon = UIImage(named: "featureBar_ribbon")!
+    static let featureBarNone = UIImage(named: "featureBar_none")!
+    
+    // 캐릭터 색
+    static let colorNavy = UIImage(named: "color_navy")!
+    static let colorMauve = UIImage(named: "color_mauve")!
+    static let colorPink = UIImage(named: "color_pink")!
+    static let colorGreen = UIImage(named: "color_green")!
+    static let colorYellow = UIImage(named: "color_yellow")!
+}
